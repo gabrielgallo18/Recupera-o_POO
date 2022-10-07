6b26cd1050457595eb74600552c2ed665d058668
gabrielgallo18 <96590617+gabrielgallo18@users.noreply.github.com>
2022-10-07 13:49:50 -0300

    Add files via upload

---
+++ Herança/car.java
@@ -0,0 +1,7 @@
+public class Car extends Veiculo {
+
+	public Car(String varModelo, String varMarca) {
+		super(varModelo, varMarca);
+	}
+
+}
---
+++ Herança/main.java
@@ -0,0 +1,8 @@
+public class main {
+
+	public static void main(String args[]) {
+		Car car = new Car("Fiesta", "Ford");
+        System.out.println("O "+caro.modelo+"MARCA"+car.marca);
+	}
+
+}
